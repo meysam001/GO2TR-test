2ca7d456135cdfbf3e16f56539165b9ec71d79f2
Meysam Mahmoudi <meysam0011212@gmail.com>
2021-02-06 22:43:42 +0330

    add login to swagger

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,28 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     operationId="/api/login",
+     *     summary="Authentication and get token",
+     *     tags={"Login"},
+     *     @OA\RequestBody(
+     *          @OA\MediaType(mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  @OA\Property(property="email", type="string", collectionFormat="multi", @OA\Items(type="string")),
+     *                  @OA\Property(property="password", type="string", collectionFormat="multi", @OA\Items(type="string")),
+     *                  required={"email", "password"}
+     *             )
+     *         )
+     *      ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Get Token",
+     *         @OA\JsonContent()
+     *     )
+     * )
+     */
     public function authenticate(Request $request)
     {
         $credentials = $request->only('email', 'password');
